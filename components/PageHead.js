f7364abff4a715630150333efab1dea83e51114f
Tom Söderlund <tom@tomsoderlund.com>
2019-12-14 14:12:28 +0100

    PageHead config

--- components/PageHead.js
+++ components/PageHead.js
@@ -3,10 +3,10 @@ import Head from 'next/head'
 import manifest from '../public/manifest.json'
 import { config } from '../config/config'
 
-const PageHead = ({ title, description = config.appDescription }) => {
+const PageHead = ({ title, appName = config.appName, appTagline = config.appTagline, appDescription = config.appDescription }) => {
   const pageTitle = title
     ? `${title} – ${config.appName}`
-    : `${config.appName} – ${config.appTagline}`
+    : `${appName} – ${appTagline}`
 
   const iconUrl = '/icon.png'
   const fonts = ['Source+Sans+Pro:400,600']
@@ -14,7 +14,7 @@ const PageHead = ({ title, description = config.appDescription }) => {
   return (
     <Head>
       <title>{pageTitle}</title>
-      <meta name='description' content={description} />
+      <meta name='description' content={appDescription} />
 
       <meta charSet='utf-8' />
       <meta httpEquiv='content-language' content={config.locale.split('_')[0]} />
@@ -28,15 +28,15 @@ const PageHead = ({ title, description = config.appDescription }) => {
 
       <link rel='shortcut icon' type='image/x-icon' href={iconUrl} />
 
-      <meta property='og:site_name' content={config.appName} />
+      <meta property='og:site_name' content={appName} />
       <meta property='og:title' content={pageTitle} />
-      <meta property='og:description' content={description} />
+      <meta property='og:description' content={appDescription} />
       <meta property='og:locale' content={config.locale} />
 
       <link rel='apple-touch-icon' href={iconUrl} />
       {(manifest.display === 'standalone') ? <meta name='apple-mobile-web-app-capable' content='yes' /> : null}
       <meta name='apple-mobile-web-app-status-bar-style' content='black-translucent' />
-      <meta name='apple-mobile-web-app-title' content={config.appName} />
+      <meta name='apple-mobile-web-app-title' content={appName} />
 
       {/*
       <link rel='apple-touch-startup-image' href='' />
